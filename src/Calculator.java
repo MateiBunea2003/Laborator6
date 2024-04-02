b5c6a9ef856fa8ded64950bb5ef775f4cc942bc3
MateiBun2003 <152338394+MateiBun2003@users.noreply.github.com>
2024-04-02 11:09:39 +0300

    Prima problema

---
+++ src/Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+    protected float value;
+
+    public Calculator(float value) {
+        this.value = value;
+    }
+
+    public void add(float ather) {
+        this.value+= ather;
+    }
+
+    public void inmultire(float ather) {
+        this.value *= ather;
+    }
+    public void imparte(float ather){
+        this.value=ather;
+    }
+    public float getresult(){
+        return this.value;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[]args){
+        Calculator c = new Calculator(5);
+        c.add(3);
+        c.inmultire(2);
+        c.imparte(4);
+        System.out.println(c.getresult());
+    }
+}
